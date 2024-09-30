8b00e6b08ef4a5af391dd07f9113ced18ac9d4fe
Shayan Khan <neonclanflamez@gmail.com>
2024-09-29 23:27:53 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// COMSC-210 | Lab 14 | Shayan Khan
+
+#include <iostream>
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+
+    Color(int r = 0, int g = 0, int b = 0) : red(r), green(g), blue(b) {}
+
+
+
+};
