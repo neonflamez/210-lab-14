24d56d2967c57136a2acbc16c455706cdb400598
Shayan Khan <neonclanflamez@gmail.com>
2024-09-29 23:41:08 -0800

    Second commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 class Color {
 private:
     int red;
@@ -12,6 +14,36 @@ public:
 
     Color(int r = 0, int g = 0, int b = 0) : red(r), green(g), blue(b) {}
 
+    void setRed(int r){
+        red = r;
+    }
 
+    void setGreen(int g){
+        green = g;
+    }
 
+    void setBlue(int b){
+        blue = b;
+    }
+
+    int getRed() const{
+        return red;
+    }
+
+    int getGreen() const{
+        return green;
+    }
+
+    int getBlue() const{
+        return blue;
+    }
+
+    void print() const{
+        cout << "Color (R = " << red << ", G = " << green << ", B = " << blue << ")" << endl;
+    }
 };
+
+int main(){
+
+    return 0;
+}
